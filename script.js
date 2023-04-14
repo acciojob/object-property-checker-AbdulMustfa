d8c449bf56b81d450fd44b9e1bf62b0ca66dc4ff
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-04-14 22:38:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code her
-	  return key in window.sampleObject;
 	return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
-
+console.log(hasKey("red"));   
+console.log(hasKey("blue"));   
+console.log(hasKey("white"));
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
